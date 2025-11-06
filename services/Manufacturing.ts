fa93d095099c695bb8f2768ad306f6e0810ca99d
abibflores <gon_abib@hotmail.com>
2025-11-05 19:10:16 -0600

    refactor: update getManufacturing function to return structured data and enhance error handling

--- services/Manufacturing.ts
+++ services/Manufacturing.ts
@@ -1,12 +1,24 @@
 import axiosInstance from "@/lib/axios-instance";
 
 export const getManufacturing = async () => {
-    try {
-      const {data} = await axiosInstance.get(
-        `/api/manufacturing/`
-      );
-      return data;
-    } catch (error) {
-      console.error(error);
-    }
+  try {
+    const {
+      data: { data, ...rest },
+    } = await axiosInstance.get(`/api/manufacturing/`);
+    return {
+      data: data.map((manufacturing: any) => ({
+        process: manufacturing.name,
+        idFacility: manufacturing.id,
+        idTypeEquipment: manufacturing.equipment_name,
+        idTypeFuelUsed: manufacturing.emission_factor_name,
+        idTypeEquipmentCode: manufacturing.id,
+        active: 1,
+        idControlManufacturing: manufacturing.id,
+        idUserControl: 66,
+      })),
+      ...rest,
+    };
+  } catch (error) {
+    console.error(error);
   }
+};
